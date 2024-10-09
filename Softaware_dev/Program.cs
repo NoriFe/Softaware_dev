8c57adfdfb3f2ed094c16169b9593fd46b4d6638
Norbert Skwierczynski <norbert.fe.dev@gmail.com>
2024-10-09 18:34:38 +0100

    adding menu to the project

--- Softaware_dev/Program.cs
+++ Softaware_dev/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Softaware_dev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   // creating menu for user with 2 options and exit
+            int option;
+
+            do
+            {
+                Console.WriteLine("Pick an option:");
+                Console.WriteLine("1. Create username");
+                Console.WriteLine("2. Calculate Factorial");
+                Console.WriteLine("3. Exit");
+                Console.Write("Enter your option: ");
+                option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        
+                        break;
+                    case 2:
+                        
+                        break;
+                    case 3:
+                        Console.WriteLine("See ya later aligator!");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option! Try agian :)");
+                        break;
+                }
+
+            }
+            while (option != 3);
+        }
+    }
+}
